1256414c8a3a6289e086a553b9889be95ab23b44
Pavel Klochkov <paul.klochkov@gmail.com>
2021-03-19 19:00:41 +0100

    Add more styles and icons

--- components/events/EventItem.jsx
+++ components/events/EventItem.jsx
@@ -1,6 +1,8 @@
-import Link from "next/link";
-
 import styles from "./EventItem.module.css";
+import Button from "../ui/Button";
+import DateIcon from "../icons/DateIcon";
+import AddressIcon from "../icons/AddressIcon";
+import ArrowRightIcon from "../icons/ArrowRightIcon";
 
 export default function EventItem({ title, image, date, location, id }) {
   const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
@@ -18,14 +20,21 @@ export default function EventItem({ title, image, date, location, id }) {
         <div className={styles.summary}>
           <h2>{title}</h2>
           <div className={styles.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={styles.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={styles.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={styles.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
---
+++ components/ui/Button.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+import styles from "./Button.module.css";
+
+export default function Button({ link, children }) {
+  return (
+    <Link href={link}>
+      <a className={styles.btn}>{children}</a>
+    </Link>
+  );
+}
