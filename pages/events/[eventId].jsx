386d642bacb240a7f7af2588c014d98624552269
Pavel Klochkov <paul.klochkov@gmail.com>
2021-03-19 19:16:06 +0100

    Master Deatil event page

--- pages/events/[eventId].jsx
+++ pages/events/[eventId].jsx
@@ -1,7 +1,31 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import { Fragment } from "react";
+import EventSummary from "../../components/event-detail/EventSummary";
+import EventLogistics from "../../components/event-detail/EventLogistics";
+import EventContent from "../../components/event-detail/EventContent";
+
 export default function EventDetailPage() {
+  const router = useRouter();
+  const { eventId } = router.query;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found</p>;
+  }
+
   return (
     <div>
-      <h1>Event Details</h1>
+      <Fragment>
+        <EventSummary title={event.title} />
+        <EventLogistics
+          date={event.date}
+          address={event.location}
+          image={event.image}
+          imageAlt={event.title}
+        />
+        <EventContent>{event.description}</EventContent>
+      </Fragment>
     </div>
   );
 }
