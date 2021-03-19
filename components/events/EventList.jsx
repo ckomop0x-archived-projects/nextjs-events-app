955e70a8d21bbc55f4143a3cf41ea74ee11366fe
Pavel Klochkov <paul.klochkov@gmail.com>
2021-03-19 18:41:47 +0100

    Add EventsList

--- components/events/EventList.jsx
+++ components/events/EventList.jsx
@@ -3,8 +3,15 @@ import EventItem from "./EventItem";
 export default function EventList({ items }) {
   return (
     <ul>
-      {items.map(({ title }) => (
-        <EventItem title={title} />
+      {items.map(({ title, image, date, location, id }) => (
+        <EventItem
+          key={id}
+          title={title}
+          image={image}
+          date={date}
+          location={location}
+          id={id}
+        />
       ))}
     </ul>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,12 @@
 import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/EventList";
 
 export default function HomePage() {
   const featuredEvents = getFeaturedEvents();
 
   return (
     <div>
-      <h1>Home Page</h1>
+      <EventList items={featuredEvents} />
     </div>
   );
 }
